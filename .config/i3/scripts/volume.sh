62c259ac6850f79aa415cb91a962e60e4a6a9cb4
imadUO4 <ouchaibimad000@gmail.com>
2021-06-24 11:07:24 +0100

    added sway

--- .config/i3/scripts/volume.sh
+++
@@ -1,9 +0,0 @@
-# bin/bash
-
-VOL=$(awk -F"[][]" '/Left:/ { print $2 }' <(amixer -D pulse sget Master))
-MAX="100%"
-if [ "$VOL" = "$MAX" ] 
-then
-		amixer -D pulse sset Master 100%
-fi		
-echo $VOL

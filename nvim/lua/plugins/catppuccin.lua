fa487a56a242aa132f495a9b7e5d667fcad14cd5
IOU4 <ouchaibimad000@gmail.com>
2023-03-10 14:22:27 +0100

    update catppuccin config

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -1,38 +1,38 @@
 return {
 	"catppuccin/nvim",
 	name = "catppuccin",
-	config = function()
-		require("catppuccin").setup({
-			flavour = "mocha", -- latte, frappe, macchiato, mocha
-			background = { light = "latte", dark = "mocha" },
-			transparent_background = true,
-			term_colors = true,
-			styles = {
-				comments = { "italic" },
-				properties = { "italic" },
-				functions = { "italic", "bold" },
-				keywords = { "italic" },
-				operators = { "bold" },
-				conditionals = { "bold" },
-				loops = { "bold" },
-				booleans = { "bold", "italic" },
-			},
-			integrations = {
-				treesitter = true,
-				native_lsp = { enabled = true },
-				lsp_trouble = true,
-				telescope = true,
-				nvimtree = true,
-				mason = true,
-				cmp = true,
-			},
-			custom_highlights = function(colors)
-				return {
-					Folded = { fg = colors.surface2, bg = colors.base },
-				}
-			end,
-		})
-
+	opts = {
+		flavour = "mocha", -- latte, frappe, macchiato, mocha
+		background = { light = "latte", dark = "mocha" },
+		transparent_background = true,
+		term_colors = true,
+		styles = {
+			comments = { "italic" },
+			properties = { "italic" },
+			functions = { "italic", "bold" },
+			keywords = { "italic" },
+			operators = { "bold" },
+			conditionals = { "bold" },
+			loops = { "bold" },
+			booleans = { "bold", "italic" },
+		},
+		integrations = {
+			treesitter = true,
+			native_lsp = { enabled = true },
+			lsp_trouble = true,
+			telescope = true,
+			nvimtree = true,
+			mason = true,
+			cmp = true,
+		},
+		custom_highlights = function(colors)
+			return {
+				Folded = { fg = colors.surface2, bg = colors.base },
+			}
+		end,
+	},
+	config = function(_, opts)
+		require("catppuccin").setup(opts)
 		vim.cmd.colorscheme("catppuccin")
 	end,
 }

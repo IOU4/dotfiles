fa43d1da301b5352c6f8b2f3f49a2977aac07ba4
IOU4 <ouchaibimad000@gmail.com>
2023-03-10 16:18:15 +0100

    lazy load neo-tree with keymap

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -1,9 +1,8 @@
-local keymap = vim.api.nvim_set_keymap
-local opts = { noremap = true, silent = true }
-keymap("n", "<leader>e", "<cmd>Neotree toggle<cr>", opts)
-
 return {
 	"nvim-neo-tree/neo-tree.nvim",
+	keys = {
+		{ "<leader>e", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
+	},
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended

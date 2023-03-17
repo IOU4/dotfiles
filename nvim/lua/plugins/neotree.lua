85881457ba48fd5934ad25966052631bbd2c93a7
IOU4 <ouchaibimad000@gmail.com>
2023-03-17 18:32:35 +0100

    update

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -8,4 +8,17 @@ return {
 		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
 		"MunifTanjim/nui.nvim",
 	},
+	opts = {
+		popup_border_style = "rounded",
+		default_component_configs = {
+			icon = {
+				folder_closed = "󰉋",
+				folder_open = "󰝰",
+				folder_empty = "󰉘",
+			},
+		},
+		filesystem = {
+			hijack_netrw_behavior = "open_default",
+		},
+	},
 }
